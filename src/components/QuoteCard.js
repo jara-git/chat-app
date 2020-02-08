6f4da41f82c713489c1b8a4d3ca1cb52437e1a55
solar <jarabelmonte@gmail.com>
2020-02-09 00:34:23 +0100

    quotecard

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -6,7 +6,17 @@ const Quote = ()=>{
     return (
     
     <div class="QuoteCard">
-        
+          <figure class="QuoteCard">
+    <img src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72"
+      alt="Nelson Muntz"/>
+    <figcaption>
+      <blockquote>
+        Shoplifting is a victimless crime, like punching someone in the dark.
+      </blockquote>
+      <cite>Nelson Muntz</cite>
+    </figcaption>
+  </figure>
+
       
     </div>
   
