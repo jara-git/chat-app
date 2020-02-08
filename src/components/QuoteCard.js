4a3779f7cd3bebf2390f9d52574c49b3e05f6b8e
solar <jarabelmonte@gmail.com>
2020-02-09 00:56:46 +0100

    all quotes

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -5,22 +5,37 @@ const Quote = ()=>{
 
     return (
     
-    <div class="QuoteCard">
-          <figure class="QuoteCard">
-    <img src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72"
-      alt="Nelson Muntz"/>
-    <figcaption>
-      <blockquote>
-        Shoplifting is a victimless crime, like punching someone in the dark.
-      </blockquote>
-      <cite>Nelson Muntz</cite>
-    </figcaption>
-  </figure>
-
+        <div>
+            <div class="QuoteCard">
+                <figure class="QuoteCard">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRqUuyCEmsgq_bJCXWw-NFwvrG39FY2PJPGoOlz9MExIvcmbTFr" alt="Nelson Muntz"/>
+                <figcaption>
+                <blockquote> Shoplifting is a victimless crime, like punching someone in the dark. </blockquote>
+                <cite>Nelson Muntz</cite>
+                </figcaption>
+                </figure>
+            </div>
+            <div class="QuoteCard">
+                <figure class="QuoteCard">
+                <img src="https://upload.wikimedia.org/wikipedia/en/1/14/Ralph_Wiggum.png" alt="Ralph Wiggun"/>
+                <figcaption>
+                <blockquote> Me fail English? That's unpossible. </blockquote>
+                <cite>Ralph Wiggun</cite>
+                </figcaption>
+                </figure>
+            </div>
+            <div class="QuoteCard">
+                <figure class="QuoteCard">
+                <img src="https://upload.wikimedia.org/wikipedia/en/5/56/Mr_Burns.png" alt="Mr Burns"/>
+                <figcaption>
+                <blockquote> Family, religion, friendship. These are the three demons you must slay if you wish to succeed in busniess.</blockquote>
+                <cite>Mr Burns</cite>
+                </figcaption>
+                </figure>
+            </div>
+        </div>
       
-    </div>
-  
-  );
+    );
 
 }
 
