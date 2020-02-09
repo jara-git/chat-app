6f75ad2c0c42cb81d0eaa92826188f83adc1e0b3
solar <jarabelmonte@gmail.com>
2020-02-09 15:33:56 +0100

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,6 @@
+/* to generate a display, using the following properties :nameavatar for the imageonline, Boolean which will be used to display, depending on its value:the text online or offlinethe small green or red status indicator 
+The CSS can inform you about the structure to give to the component, but here are some additional clues:The Contact class will be applied to the tag that opens and closes the component (enclosing tag).This is not apparent in the CSS, but you will probably need a div to surround the right part (name + status).The `status' class will be applied to a tag that will include the status indicator (red or green) and the corresponding text.The status indicator can only have one class at a time (status-online or status-offline).
+
+•
+Displays this component three times from the App component, with different values: you can retrieve names and images from this Random User Generator (reload the page to get a new user).*/
+
