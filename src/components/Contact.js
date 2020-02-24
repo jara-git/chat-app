feda88cf1451196913e7b0e1874ab5f9f7880196
solar <jarabelmonte@gmail.com>
2020-02-25 00:08:13 +0100

    check online chat app

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Contact.scss';
 
+export default class Contact extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      online: false
+    }
+  }
 
-const Contact = (props)=>{
-  return(
-    <div className="Contact">
-      <img src={props.image} alt={props.name} className="avatar"/>
-      <div>
-        <p className="name">{props.name}</p>
-        <div className="status">
-            {
-              props.onlineStatus
-              ? <div>
-                  <div className="status-online"></div>
-                  <p className="status-text">online</p>
-                </div>
-              : <div>
-                  <div className="status-offline"></div>
-                  <p className="status-text">offline</p>
-                </div>
-            }
+  omponentDidMount(){
+    this.setState({
+      online: this.props.onlineStatus
+    })
+  }
+
+  changeStatus = () => {
+    const clicker= !this.state.online
+    this.setState({
+      online: clicker
+    })
+  }
+
+  render(){
+    return(
+      <div className="Contact">
+        <img src={ this.props.image } alt={ this.props.name } className="avatar"/>
+        <div>
+          <p className="name">{ this.props.name }</p>
+          <div className="status" onClick={ this.changeStatus } >
+              {
+                this.state.online
+                ? <div>
+                    <div className="status-online"></div>
+                    <p className="status-text">online</p>
+                  </div>
+              :   <div>
+                    <div className="status-offline"></div>
+                    <p className="status-text">offline</p>
+                  </div>
+              }
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 
-export default Contact
 
 
 
